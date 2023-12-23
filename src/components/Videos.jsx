a414b369b460eab9bd0aef980b7e19f909d130a3
priyamuskan-2901 <muskan.priya.civ21@itbhu.ac.in>
2023-12-24 00:18:12 +0530

    initial commit

---
+++ src/components/Videos.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Stack, Box } from "@mui/material";
+
+import { ChannelCard, Loader, VideoCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+  if(!videos?.length) return <Loader />;
+
+  const validVideos = videos.filter(item => item.id.videoId || item.id.channelId);
+  
+  return (
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {validVideos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+}
+
+export default Videos;
